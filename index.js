8b3b66fc552bafea992a4dcd793b34d23606c714
Nick Landas <nick.landas@gmail.com>
2022-11-01 10:14:45 -0500

    commit tree f908c3f5d26de6d0cda180c2fe411543b7eedd6f

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      
+    }
+  
+    return gifts;
+}
+  
+wrapGifts(gifts); 
+
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const message = [];
+const gathering = 'surprise';
+const x = names.length;
+function writeCards(message) {
+    for(let i = 0; i < x; i++) {
+        message[i] = (`Thank you, ${names[i]}, for the wonderful ${gathering} gift!`);
+        //console.log(`Thank you, ${names[i]} for the wonderful ${gathering} gift!`);
+    }
+    return message;
+}
+writeCards(message);
+
+
+function countDown(xy) {
+    while (xy >= 0) {
+        console.log(xy--);
+    }
+}
+countDown(10);
